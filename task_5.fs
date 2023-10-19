fa0302be208cdf67d5444410b6a2208e99e29251
a-max-22 <108484061+a-max-22@users.noreply.github.com>
2023-10-19 12:34:12 +0300

    Update task_5.fs

--- task_5.fs
+++ task_5.fs
@@ -1,13 +1,15 @@
 // 16.1
-let notDivisible (n,m) = ( (m % n) = 0 )  
+let notDivisible (n,m) = not ( (m % n) = 0 )  
 
-let checkAllDivisors (n, currentDivisor) = 
+let rec checkAllDivisors (currentDivisor, n) =  
     match currentDivisor with
         | 2 -> notDivisible (2, n)
-        | currentDivisor -> notDivisible (currentDivisor, n) || notDivisible (currentDivisor - 1, n)      
+        | currentDivisor -> notDivisible (currentDivisor, n) && checkAllDivisors(currentDivisor - 1, n)      
 
 // 16.2
-let  prime n = function
-    | 1 -> false
-    | 2 -> true
-    | n -> checkAllDivisors(n-1, n) 
+let  prime n = 
+    match n with
+        | 1 -> false
+        | 2 -> true
+        | n when n > 2 -> checkAllDivisors(n-1, n) 
+        | n  -> false 
