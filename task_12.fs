347dc2d65c93b295b6b4c90c0462f09c75d23c69
a-max-22 <108484061+a-max-22@users.noreply.github.com>
2023-11-18 16:05:59 +0300

    Create task_12.fs

---
+++ task_12.fs
@@ -0,0 +1,20 @@
+// 34.1
+let rec upto n =
+    let rec pass (n,i) = 
+        match i with
+            | 0 -> []
+            | _ -> n - i + 1 :: pass(n, i-1)
+    pass (n,n)
+
+// 34.2
+let rec dnto n = 
+    match n with
+        | 0 -> []
+        | _ -> n :: dnto(n-1)
+// 34.3
+let rec evenn n = 
+    let rec pass1 (n,i) =
+        match i with
+            | 0 -> []
+            | i -> (n - i + 2) :: pass1(n, i - 2)
+    pass1 (2*n, 2*n)
