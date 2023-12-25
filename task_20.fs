10902bb10fdef3926e89aec3f96f177394163ca1
a-max-22 <108484061+a-max-22@users.noreply.github.com>
2023-12-25 12:34:15 +0300

    Create task_20.fs

---
+++ task_20.fs
@@ -0,0 +1,13 @@
+// 49.5.1
+let even_seq = Seq.initInfinite (fun i -> 2*i)
+
+let rec fact = function 
+        | 0 -> 1
+        | n when n > 0 ->  n * (fact (n-1))
+        | _ -> 0
+        
+// 49.5.2
+let fac_seq = Seq.initInfinite(fun i -> fact i)
+
+// 49.5.3
+let seq_seq = Seq.initInfinite(fun i -> ((i / 2) + (i % 2)) * (pown (-1) i) )
