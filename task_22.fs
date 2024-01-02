2abec3e1835b901fbcf3bb235418a4308eef1e69
a-max-22 <108484061+a-max-22@users.noreply.github.com>
2024-01-02 22:27:05 +0300

    Create task_22.fs

---
+++ task_22.fs
@@ -0,0 +1,20 @@
+
+type 'a cell = Nil | Cons of 'a * Lazy<'a cell>
+
+let hd (s : 'a cell) : 'a =
+  match s with
+    Nil -> failwith "hd"
+  | Cons (x, _) -> x
+
+let tl (s : 'a cell) : Lazy<'a cell> =
+  match s with
+    Nil -> failwith "tl"
+  | Cons (_, g) -> g
+
+
+// 51.3
+let rec nth (s : 'a cell) (n : int) : 'a =
+  match n with
+      | 0 -> hd s
+      | _ -> let t = (tl s) 
+             nth (t.Force()) (n-1)
